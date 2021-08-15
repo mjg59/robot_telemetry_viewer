2e220047a4aab094c68477d17cdd00534a794ad7
Matthew Garrett <mjg59@srcf.ucam.org>
2021-08-15 12:17:47 -0700

    Clear the screen every second
    Deal with any garbage that other components may drop onto the screen

--- visualise.py
+++ visualise.py
@@ -13,6 +13,7 @@ from_file = None
 ser = None
 term = Terminal()
 dumpmode = None
+last_update = 0
 
 parameters = [[{'name': "temp_fet", 'warn': 80, 'critical': 100}],
               [{'name': "temp_fet", 'warn': 80, 'critical': 100}],
@@ -141,14 +142,15 @@ else:
     ser = serial.Serial("/dev/ttyUSB0", 115200)
     output = open("telemetry.%d" % int(time.time()), "wb")
 
-if dumpmode == None:
-    print(term.on_black + term.home + term.clear, end='')
-    print(term.white_on_black + "VESC 0" + term.normal, end='')
-    print(term.move_x(int(term.width * 0.25)) + term.white_on_black + "VESC 1" + term.normal, end='')
-    print(term.move_x(int(term.width * 0.5)) + term.white_on_black + "VESC 2" + term.normal, end='')
-    print(term.move_x(int(term.width * 0.75)) + term.white_on_black + "VESC 3" + term.normal, end='')
-
 while True:
+    if int(time.time()) - last_update > 1:
+        if dumpmode == None:
+            print(term.on_black + term.home + term.clear, end='')
+            print(term.white_on_black + "VESC 0" + term.normal, end='')
+            print(term.move_x(int(term.width * 0.25)) + term.white_on_black + "VESC 1" + term.normal, end='')
+            print(term.move_x(int(term.width * 0.5)) + term.white_on_black + "VESC 2" + term.normal, end='')
+            print(term.move_x(int(term.width * 0.75)) + term.white_on_black + "VESC 3" + term.normal, end='')
+        last_update = int(time.time())
     datatype = data_read()
     if datatype == b'\x41':
         # Accelerometer data
